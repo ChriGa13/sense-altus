062cc6d8f356d6de8ddd19817a97a550cb30e3e6
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2020-11-03 21:40:38 +0100

    Add first version of app settings (color example)

--- sense-altus/app/index.ts
+++ sense-altus/app/index.ts
@@ -1,18 +1,12 @@
 /*** UI Imports ***/
-import { display } from "display";
-import document from "document";
+import document from 'document';
+
+/*** General Imports ***/
+import * as messaging from 'messaging';
 
 /*** Sensor Imports ***/
-import { Barometer } from "barometer";
-import { geolocation, PositionError } from "geolocation";
-
-/*** Definitions ***/
-const gpsTimeout = 60_000;
-const barLabel = document.getElementById("bar-label");
-const barData = document.getElementById("bar-data");
-// const gpsLabel = document.getElementById("gps-label");
-// const gpsData = document.getElementById("gps-data");
-const sensors: any[] = [];
+import { AltusBarometer } from './features/altus-barometer';
+import { AltusGPS } from './features/altus-gps';
 
 /*** Use Localization ***/
 // https://dev.fitbit.com/build/guides/localization/
@@ -20,59 +14,52 @@ const sensors: any[] = [];
 /*** Set Settings via App ***/
 // https://dev.fitbit.com/build/guides/settings/
 
-/*** Script ***/
-if (Barometer) {
-  // https://dev.fitbit.com/build/guides/sensors/barometer/
-  const barometer = new Barometer({ frequency: 1 }); // returns atmospheric pressure in Pa (Pascal)
-  
-  if(barometer) {
-    barometer.addEventListener("reading", () => {
-    
-      // math magic: https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel
-      if(barometer.pressure) {
-        const height = Math.round((288.15 / 0.0065) * (1 - (Math.pow((barometer.pressure / (1013.25 * 100)), (1/ 5.255)))));
-      
-        if(barData) {
-          barData.text = JSON.stringify({
-            height: height + "m"
-          });
-        }
-      }
+/*** Definitions ***/
+const barLabel = document.getElementById('bar-label');
+const barData = document.getElementById('bar-data');
 
-    });
+const gpsLabel = document.getElementById('gps-label');
+const gpsData = document.getElementById('gps-data');
+
+
+/*** Barometer Script ***/
+const barometer: AltusBarometer = 
+  new AltusBarometer(
+    (barLabel as HTMLElement), 
+    (barData as HTMLElement)
+  );
+
+if(barometer.isBarometerAvailable()) {
+  barometer.addOnChangeEventListener();
+  barometer.start();
+}
+
+/*** GPS Script ***/
+const gps: AltusGPS = 
+  new AltusGPS(
+      (gpsLabel as HTMLElement), 
+      (gpsData as HTMLElement)
+  );
+
+gps.startWatching();
+
+/*** Settings Script ***/
+
+// Message is received
+messaging.peerSocket.onmessage = evt => {
+  console.log(`App received: ${JSON.stringify(evt)}`);
+  if (evt.data.key === "color" && evt.data.newValue) {
+    let color = JSON.parse(evt.data.newValue);
+    console.log(`Setting background color: ${color}`);
   }
+};
 
+// Message socket opens
+messaging.peerSocket.onopen = () => {
+  console.log("App Socket Open");
+};
 
-} else {
-  (barLabel as HTMLElement).style.display = "none";
-  (barData as HTMLElement).style.display = "none";
-}
-
-// var watchID = geolocation.watchPosition(locationSuccessCallback, locationErrorCallback, { timeout: gpsTimeout });
-
-function locationSuccessCallback(position: Position) {
-  console.log("gps interval triggered");
-  // gpsData.text = JSON.stringify({
-  //   altitude: position.coords.altitude ? position.coords.altitude : "no gps altitude"
-  // });
- 
-  console.log("Latitude: " + position.coords.latitude,
-              "Longitude: " + position.coords.longitude,
-              "Altitude: " + position.coords.altitude);
-}
-
-function locationErrorCallback(error: PositionError) {
-  console.log("Error: " + error.code,
-              "Message: " + error.message);
-}
-
-display.addEventListener("change", _ => {
-  // Automatically stop all sensors when the screen is off to conserve battery
-  if(display.on) {
-    sensors.map(sensor => sensor.start());
-    // geolocation.watchPosition(locationSuccessCallback, locationErrorCallback, { timeout: gpsTimeout });
-  } else {
-    sensors.map(sensor => sensor.stop());
-    // geolocation.clearWatch(watchID);
-  }
-});
+// Message socket closes
+messaging.peerSocket.onclose = () => {
+  console.log("App Socket Closed");
+};
