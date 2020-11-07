38993f25c3e68f5489e2680af4e43765a883e0e2
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2020-11-07 15:19:50 +0100

    Add possibility to use advanced barometric height formula (based on settings)

--- sense-altus/app/features/altus-barometer.ts
+++ sense-altus/app/features/altus-barometer.ts
@@ -1,5 +1,6 @@
 import { display } from 'display';
 import { Barometer } from 'barometer';
+import { SettingsService } from '../services/settings-service';
 
 interface NullableSensor {
     barometer?: Barometer;
@@ -9,13 +10,15 @@ export class AltusBarometer {
 
     private barLabel: HTMLElement;
     private barData: HTMLElement;
+    private settingsService: SettingsService;
 
     private sensor: NullableSensor;
     private height: number = 0;
 
     get getHeight(): number { return this.height; }
     
-    constructor(barLabel: HTMLElement, barData: HTMLElement) {
+    constructor(settingsService: SettingsService, barLabel: HTMLElement, barData: HTMLElement) {
+        this.settingsService = settingsService;
         this.barLabel = barLabel;
         this.barData = barData;
 
@@ -24,13 +27,18 @@ export class AltusBarometer {
             this.sensor = { barometer: new Barometer({ frequency: 1 }) }; // returns atmospheric pressure in Pa (Pascal)
             if(this.sensor?.barometer) {
               this.sensor.barometer.addEventListener('reading', () => {
-              
-                // math magic: https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel
                 if(this.sensor.barometer?.pressure) {
-                  this.height = Math.round((288.15 / 0.0065) * (1 - (Math.pow(((this.sensor.barometer.pressure / 100) / 1013.25), (1/ 5.255)))));
-                  this.barData.text = this.height + 'm';
+                    const settings = this.settingsService?.getCurrentSettings();
+                    if(settings?.isTemperatureEnabled?.valueOf() === true && settings?.temperature) {
+                        // TODO: use advanced barometric heigh formula
+                        this.height = Math.round((288.15 / 0.0065) * (1 - (Math.pow(((this.sensor.barometer.pressure / 100) / 1013.25), (1/ 5.255)))));
+                        this.barData.text = this.height + 'm';
+                    } else {
+                        // math magic: https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel
+                        this.height = Math.round((288.15 / 0.0065) * (1 - (Math.pow(((this.sensor.barometer.pressure / 100) / 1013.25), (1/ 5.255)))));
+                        this.barData.text = this.height + 'm';
+                    }
                 }
-          
               });
             }
           } else {
